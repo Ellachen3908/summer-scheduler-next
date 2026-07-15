b827c7c3a0e1ff54e382ff79c95e9b22d0896988
Ellachen3908 <1574740107@qq.com>
2026-07-15 15:49:17 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "暑期订课排课系统",
-  description: "家长、老师、教务后台一体化排课系统"
+title: "武博留学补课系统",
+description: "武博留学家长、老师、教务后台一体化补课排课系统"
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
